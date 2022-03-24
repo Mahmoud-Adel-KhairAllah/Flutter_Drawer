e1e22919b6be149891a83bdd17207d91e9d13ce3
MahmoudKheerAllah <ma6370612@gmail.com>
2022-03-24 14:09:04 +0200

    mm

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'drawerwidget.dart';
 
 void main() {
+  // ignore: unused_local_variable
+
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeLeft,
@@ -19,6 +25,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final oriantation = MediaQuery.of(context).orientation;
     // TODO: implement build
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 214, 112, 112),
@@ -50,6 +57,46 @@ class MyApp extends StatelessWidget {
               ],
             ),
           )),
+      body: oriantation == Orientation.landscape
+          ? MyLand()
+          : Center(
+              child: Text("hellowor1d"),
+            ),
+    );
+  }
+}
+
+class MyLand extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          height: 200,
+          width: 200,
+          child: ListView(
+            children: [
+              DrawerWidget(
+                title: "FIRST ELEMENT",
+              ),
+              DrawerWidget(
+                title: "SECOND ELEMENT",
+              ),
+              DrawerWidget(
+                title: "THIRD ELEMENT",
+              ),
+              DrawerWidget(
+                title: "FORTH ELEMENT",
+              ),
+              DrawerWidget(
+                title: "FIFTHE ELEMENT",
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
